f62a686a5145c473d0cdbd4d150674e1e797fd92
dechavezv <dechavezv@ucla.edu>
2020-11-12 14:00:51 -0800

    ouput to other direc and change mem

--- 1-MapToreRerence/08_HaplotypeCaller/run_GATK_HC_GVCFs.rails.sh
+++ 1-MapToreRerence/08_HaplotypeCaller/run_GATK_HC_GVCFs.rails.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -wd /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
-#$ -l highmem,h_rt=190:00:00,h_data=30G,highp,h_vmem=50G
+#$ -l highmem,h_rt=190:00:00,h_data=16G,highp,h_vmem=50G
 #$ -N HC_rail
 #$ -o /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/gvcf/log/reports
 #$ -e /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/gvcf/log/reports
@@ -13,10 +13,11 @@ module load java
 module load samtools
 source activate gatk-intel
                                      
-export DIREC=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
+export DIREC=/u/scratch/d/dechavez/readsRailsFulgent
+export OUT=/u/home/d/dechavez/project-rwayne/rails.project
 export BAM=${1}
 export Reference=/u/home/d/dechavez/project-rwayne/rails.project/reference.genomes/InaccesibleRail/InaccesibleRail.fa
-export temp=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/temp
+export temp=/u/scratch/d/dechavez/readsRailsFulgent/temp
 
 cd ${DIREC}
 
@@ -24,11 +25,11 @@ echo "#######"
 echo "Haplotype Caller for '$1' "
 echo "########"
 
-/u/home/d/dechavez/project-rwayne/gatk-4.1.4.1/gatk --java-options "-Xmx30G" \
+/u/home/d/dechavez/project-rwayne/gatk-4.1.4.1/gatk --java-options "-Xmx16G" \
 HaplotypeCaller \
 -R ${Reference} \
 -I ${BAM} \
--O ${DIREC}/${BAM%.bam}.g.vcf.gz \
+-O ${OUT}/${BAM%.bam}.g.vcf.gz \
 -ERC BP_RESOLUTION \
 -mbq 10 \
 --output-mode EMIT_ALL_ACTIVE_SITES \
